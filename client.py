ea7caa9f11771a8b780a0866ccc0689a5f2bf72c
Terenty Rezman <terenty.rezman@mail.ru>
2023-08-23 17:30:54 +0300

    update client

--- client.py
+++ client.py
@@ -13,20 +13,31 @@ import asyncio
 import websockets
 
 from utils import run, parse_packet
-from tun import TUNInterface
+from tun import create_tun
 
 LOGGER.basicConfig(level=LOGGER.DEBUG)
 
-# connect to server
-SERVER_ADDR = "38.242.223.247"
-SERVER_UDP_PORT = 12000
+# connect to ws server
+SERVER_ADDR = "wss://tech.divid.team/myvpn"
+SERVER_ADDR = "ws://tech.divid.team:8777"
 
 # tun interface config
 TUN_IF_NAME = "custom-tunnel"
 TUN_IF_ADDRESS = '10.1.0.1'
-    
 
-def setup_route_table(interface_name):
+
+def resolve_ip_address(addr: str):
+    # remove scheme
+    addr = re.sub("^\w*://", "", addr)
+    # remove port
+    addr = re.sub(":\d+$", "", addr)
+    # resolve if domain name 
+    if any(letter.isalpha() for letter in addr):
+        addr = socket.gethostbyname_ex(addr)[2][0]
+    return addr
+
+
+def setup_route_table(interface_name, server_ip_addr):
     # enable packet forwarding on host
     run("/usr/sbin/sysctl -w net.ipv4.ip_forward=1");
 
@@ -37,15 +48,15 @@ def setup_route_table(interface_name):
     old_gateway_ip_addr = re.search(ipv4, old_default_route)
 
     # add route to vpn server through old gateway ip
-    run(f"ip route add {SERVER_ADDR} via {old_gateway_ip_addr[0]}")
+    run(f"ip route add {server_ip_addr} via {old_gateway_ip_addr[0]}")
 
     # add default route for all trafic through our tun interface
     run(f"ip route add 0/1 dev {interface_name}");
     run(f"ip route add 128/1 dev {interface_name}");
 
 
-def cleanup_route_table():
-    run(f"ip route del {SERVER_ADDR}");
+def cleanup_route_table(server_ip_address):
+    run(f"ip route del {server_ip_address}");
     run("ip route del 0/1");
     run("ip route del 128/1");
 
@@ -56,32 +67,39 @@ def create_udp_socket():
     return s
 
 
-async def test() -> None:
-    to_server = create_udp_socket()
-    server_addr = (SERVER_ADDR, SERVER_UDP_PORT)
+async def tun_writer(tun_interface, ws_socket):
+    while True:
+        message = await ws_socket.recv()
+        print("received", message)
+        await tun_interface.write(message)
 
-    interface = await TUNInterface(TUN_IF_NAME, address=IPv4Address(TUN_IF_ADDRESS))
-    setup_route_table()
 
+async def tun_reader(tun_interface, ws_socket):
+    while True:
+        packet = await tun_interface.read(1024)
+        parsed_packet = parse_packet(packet)
+        print(parsed_packet)
+        await ws_socket.send(packet)
+
+
+async def main():
     try:
-        while True:
-            packet = await interface.read(1024)
-            parsed_packet = parse_packet(packet)
-            print(parsed_packet)
-            to_server.sendto(packet, server_addr)
+        server_ip_addr = resolve_ip_address(SERVER_ADDR) 
 
+        ws_to_server = await websockets.connect(SERVER_ADDR)
+
+        tun_interface = await create_tun(TUN_IF_NAME, IPv4Address(TUN_IF_ADDRESS))
+        setup_route_table(TUN_IF_NAME, server_ip_addr)
+
+        await asyncio.gather(
+            tun_reader(tun_interface, ws_to_server),
+            tun_writer(tun_interface, ws_to_server)
+        )
     except KeyboardInterrupt:
         pass
     finally:
-        cleanup_route_table()
-
-
-async def ws_client():
-    async with websockets.connect("ws://tech.divid.team:8765") as websocket:
-        await websocket.send("Hello world!")
-        message = await websocket.recv()
-        print(f"Received: {message}")
+        cleanup_route_table(server_ip_addr)
 
 
 if __name__ == '__main__':
-    asyncio.run(ws_client())
+    asyncio.run(main())
--- tun.py
+++ tun.py
@@ -12,34 +12,12 @@ _UNIX_IFF_TUN = 0x0001
 _UNIX_IFF_NO_PI = 0x1000
 
 
-async def test_tun():
-
-    # descriptor = os.open('/dev/net/tun', os.O_RDWR)
-    tun = await aiofiles.open('/dev/net/tun', "r+b", buffering=0)
-
-    descriptor = tun.fileno()
-
-    ioctl(
-        descriptor,
-        _UNIX_TUNSETIFF,
-        struct.pack('16sH', "TEST-TUN".encode('ASCII'), _UNIX_IFF_TUN | _UNIX_IFF_NO_PI)
-    )
-
-    # Assign address to interface.
-    subprocess.call(['/sbin/ip', 'addr', 'add', str("10.1.0.1"), 'dev', "TEST-TUN"])
-
-    # up interface
-    subprocess.call(['/sbin/ip', 'link', 'set', 'dev', "TEST-TUN", 'up'])
-
-
-    return tun
-
-
 class TUNInterface:
-    async def __init__(self, name: str, address: IPv4Address):
+    def __init__(self, name: str, address: IPv4Address):
         self._name = name
         self._address = address
-
+    
+    async def init(self):
         self._tun = await aiofiles.open('/dev/net/tun', "r+b", buffering=0)
 
         # Create TUN interface.
@@ -48,11 +26,11 @@ class TUNInterface:
         ioctl(
             descriptor,
             _UNIX_TUNSETIFF,
-            struct.pack('16sH', name.encode('ASCII'), _UNIX_IFF_TUN | _UNIX_IFF_NO_PI)
+            struct.pack('16sH', self._name.encode('ASCII'), _UNIX_IFF_TUN | _UNIX_IFF_NO_PI)
         )
 
         # Assign address to interface.
-        subprocess.call(['/sbin/ip', 'addr', 'add', str(address), 'dev', name])
+        subprocess.call(['/sbin/ip', 'addr', 'add', str(self._address), 'dev', self._name])
 
         # up interface
         subprocess.call(['/sbin/ip', 'link', 'set', 'dev', self._name, 'up'])
@@ -65,3 +43,9 @@ class TUNInterface:
     async def write(self, packet: bytes) -> None:
         # LOGGER.debug('Writing %s bytes to %s: %s', len(packet), self._name, packet[:10])
         await self._tun.write(packet)
+
+
+async def create_tun(name: str, address: IPv4Address) -> TUNInterface:
+    tun = TUNInterface(name, address)
+    await tun.init()
+    return tun
