6a6931666c9af2f6d5a7a85cc60f2ef7b77b4f32
Terenty Rezman <terenty.rezman@mail.ru>
2023-08-23 17:27:07 +0200

    sever wont forward

--- server.py
+++ server.py
@@ -15,13 +15,13 @@ TUN_IF_NAME = "custom-tunnel"
 TUN_IF_ADDRESS = '10.1.0.2'
 
 
-def setup_route_table(interface_name, server_ip_addr):
+def setup_route_table(interface_name):
     run("iptables -t nat -A POSTROUTING -s 10.1.0.0/24 ! -d 10.1.0.0/24 -m comment --comment 'vpndemo' -j MASQUERADE");
     run("iptables -A FORWARD -s 10.1.0.0/24 -m state --state RELATED,ESTABLISHED -j ACCEPT");
     run("iptables -A FORWARD -d 10.1.0.0/24 -j ACCEPT");
 
 
-def cleanup_route_table(server_ip_address):
+def cleanup_route_table():
     run("iptables -t nat -D POSTROUTING -s 10.1.0.0/24 ! -d 10.1.0.0/24 -m comment --comment 'vpndemo' -j MASQUERADE");
     run("iptables -D FORWARD -s 10.1.0.0/24 -m state --state RELATED,ESTABLISHED -j ACCEPT");
     run("iptables -D FORWARD -d 10.1.0.0/24 -j ACCEPT");
@@ -36,7 +36,7 @@ async def handle_client(tun_interface, websocket):
 async def tun_writer(tun_interface, ws_socket):
     while True:
         message = await ws_socket.recv()
-        print("received", message)
+        print("WRITE TO TUN", message)
         await tun_interface.write(message)
 
 
@@ -44,14 +44,14 @@ async def tun_reader(tun_interface, ws_socket):
     while True:
         packet = await tun_interface.read(1024)
         parsed_packet = parse_packet(packet)
-        print(parsed_packet)
+        print("READ FROM TUN", parsed_packet)
         await ws_socket.send(packet)
 
 
 async def ws_server():
     try:
-        tun_interface = create_tun(TUN_IF_NAME, IPv4Address(TUN_IF_ADDRESS))
-        setup_route_table()
+        tun_interface = await create_tun(TUN_IF_NAME, IPv4Address(TUN_IF_ADDRESS))
+        setup_route_table(TUN_IF_ADDRESS)
 
         async with websockets.serve(partial(handle_client, tun_interface), "0.0.0.0", 8777):
             print("listening...")
